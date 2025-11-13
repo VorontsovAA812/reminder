761b94d617ff8d98bcfd034d1cab7638fbf0117e
79033 <andreylenkom32@gmail.com>
2025-11-13 17:28:20 +0400

    Подкорректировал структуру проекта. Исправил мелкие недочеты в проектировании rest

--- src/main/java/com/reminder/app/controller/UserController.java
+++ src/main/java/com/reminder/app/controller/UserController.java
@@ -3,7 +3,11 @@ package com.reminder.app.controller;
 import com.reminder.app.repository.UserRepository;
 import com.reminder.app.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,17 +15,28 @@ import java.util.List;
 @RestController
 public class UserController {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Autowired
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    @GetMapping("/user/all")
+    @GetMapping("/users/all")
     public List<User> allUsers() {
         return userRepository.findAll();
     }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        User saved = userRepository.save(user);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(saved);
+    }
+
+
+
 
 
 }
